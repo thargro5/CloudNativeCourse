477dc5a3c34a8e77099d0e352056652a2f1d8cc4
thargro5 <thargro5@uncc.edu>
2024-03-11 18:42:05 -0400

    new one the old one isnt updated

--- lab6/main.go
+++ lab6/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"gitlab.com/arunravindran/cloudnativecourse/lab6-external-rest-api/weather"
+	"github.com/thargro5/cloudnativecourse/lab6-external-rest-api/weather"
 )
 
 func main() {
